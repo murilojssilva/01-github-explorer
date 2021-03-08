76457fb6952941b3a2af0bc06c407f9a7bb56577
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2021-03-08 14:35:52 -0300

    Aula 18 - Listando repositorios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem({repository,description,link}){
 	return (
 		<li>
-			<strong>{repository.name ?? "Default"}</strong>
-			<p>{repository.description ?? "Sem descrição"}</p>
-			<a href={repository.link ?? "#"}>Acessar repositório</a>
+			<strong>{repository.name}</strong>
+			<p>{repository.description}</p>
+			<a href={repository.html_url}>Acessar repositório</a>
 		</li>
 	)
 }
